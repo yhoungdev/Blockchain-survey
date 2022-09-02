1a03b795d242a892ddf471b01ff8cf20ed78347d
Obiabo Immanuel <54102389+yhoungdev@users.noreply.github.com>
2022-09-02 10:14:59 +0100

    feat: finished button

--- src/pages/Verification.tsx
+++ src/pages/Verification.tsx
@@ -7,12 +7,17 @@ import {AiOutlineUsergroupAdd} from 'react-icons/ai'
 import StepContainer from "../Layouts/StepContainer"
 import { useState, useEffect } from "react"
 import {FiMail} from 'react-icons/fi'
+import axios from 'axios'
 //@ts-ignor
 
 export const Verification =() => {
 
     const [ data  , setData ] = useState('');
     const [ color , setColor ] = useState()
+    const [ disabled , setDisabled ] = useState(true)
+    const [ hide , setHide ] = useState('')
+    const [email , setEmail ] = useState()
+    const [loader , setLoader ] = useState(false )
 
     //function to handle mutiple 
     const handleMultiple =() => {
@@ -28,6 +33,49 @@ export const Verification =() => {
     }
     
 
+    //handle onChange for input
+    //@ts-ignor
+    //eslint-disable-next-line
+   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+     // @ts-ignore *
+    const handleInput = e => {
+
+        const val = e.target.value ;
+            //check if value is empthy 
+            if(val <= 0|| val === null || val === '') {
+                setDisabled(true )
+            } else {
+                setDisabled(false )
+                setEmail(val)
+            }
+            console.log(val)
+    }
+
+
+    //submit item 
+    const handleRequest =  () => {
+        //handle Api submit here 
+        setLoader(true)
+
+        // send data to api
+        const url=`https://v1.nocodeapi.com/obiabo/telegram/XNTveTVLNizpHNLw/sendText?text=${email}`
+        try {
+            const request = axios.post( url , {
+                text: email
+            } );
+
+            setTimeout(() => {
+                setLoader(false)
+                 setHide('none');
+                 setDisabled(true)
+
+            }, 2500)
+
+        } catch( error ) {
+            console.warn(error)
+        }
+        
+    }
 
     useEffect(() => {
 
@@ -93,13 +141,27 @@ export const Verification =() => {
                             <>
 
                                 <form>
-                                    <InputGroup>
-                                        <Input placeholder={'Add a Co-founder'} my={'0.5em'} required/>
+                                    <Text my={'1em'} fontWeight={'bold'} display={ !hide ? 'none' : 'block' }>Request Sent to {email} (Sol: 0) <span style={{color:'red'}}>Unverified</span> </Text>
+                                    <InputGroup display={hide}>
+                                    <Input 
+                                         type={'email'}
+                                         placeholder={'Add a Co-founder'}  
+                                            onChange={handleInput}
+                                        my={'0.5em'} required/>
                                         <InputLeftElement children={<FiMail/>} py={'1.6em'} />
                                     </InputGroup>
-                                    <Buttons bg={'#7f58e2'} _hover={{bg:'black'}} color={'#fff'} fontWeight={'bold'}>
+
+
+                                    <Button w={'100%'} bg={'#7f58e2'}
+                                     _hover={{bg:'purple.600'}} color={'#fff'}
+                                      fontWeight={'bold'}
+                                      disabled={disabled}
+                                      onClick={handleRequest}
+                                      isLoading={loader}
+                                      loadingText={'Please Wait....'}
+                                      >
                                         Add Co-Founder 
-                                    </Buttons>
+                                    </Button>
                                 </form>
                             
                             </>
