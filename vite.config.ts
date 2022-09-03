b061e9156bf82b50e0c09ee3e76fc2ffcbb0f550
Obiabo Immanuel <54102389+yhoungdev@users.noreply.github.com>
2022-09-03 10:39:32 +0100

    feat: fixed buffer

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 // https://vitejs.dev/config/
 // export default defineConfig({
 //   plugins: [react()]
@@ -14,6 +13,14 @@ export default defineConfig({
   optimizeDeps: {
     esbuildOptions: { target: "es2020", supported: { bigint: true } }
   },
-  plugins: [react()]
+  define: {
+    global: 'globalThis'
+},
+  plugins: [
+    react(),
+    NodeGlobalsPolyfillPlugin({
+      buffer: true
+  })
+    ]
 // plugins: [reactRefresh()]
 })
